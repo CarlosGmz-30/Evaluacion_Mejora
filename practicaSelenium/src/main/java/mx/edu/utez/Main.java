c03c1f1071128104c04aaf786a6017fd204f1271
Carlos <20223tn066@utez.edu.mx>
2023-11-26 19:33:04 -0600

    Comentarios sobre cada acción realizada

--- practicaSelenium/src/main/java/mx/edu/utez/Main.java
+++ practicaSelenium/src/main/java/mx/edu/utez/Main.java
@@ -3,9 +3,12 @@ package mx.edu.utez;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -20,21 +23,31 @@ public class Main {
         navegador.manage().window().maximize();
         navegador.get("http://localhost:8080/intCapos_war_exploded/index.jsp");
 
-        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        // Presionamos el botón de usuario para inciar sesión
         navegador.findElement(By.xpath("/html/body/header/nav/ul/button[1]")).click();
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        // Llenamos el input de correo
         navegador.findElement(By.xpath("//*[@id=\"email-login\"]")).sendKeys("20223tn066@utez.edu.mx");
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        // Llenamos el input de contraseña
         navegador.findElement(By.xpath("//*[@id=\"contraseña-login\"]")).sendKeys("123456789");
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        // Presionamos el botón de iniciar sesión
         navegador.findElement(By.xpath("//*[@id=\"iniciar\"]")).click();
-        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        // Presionamos en el menú de categorías
         navegador.findElement(By.xpath("/html/body/nav/ul/li[1]/a")).click();
-        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        // Seleccionamos el producto a agregar al carrito
         navegador.findElement(By.xpath("//*[@id=\"categorys-container\"]/div/div/div[1]/a")).click();
-        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        // Agregtamos un scroll a la pantalla para encontrar el botón de agregar al carrito
         JavascriptExecutor js = (JavascriptExecutor) navegador;
-        js.executeScript("window.scrollBy(0,300)", "");
-        navegador.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
+        js.executeScript("window.scrollBy(0,600)", "");
+        navegador.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // Click en botón de agregar al carrito
         navegador.findElement(By.xpath("//*[@id=\"add-btn-container\"]/button")).click();
+        // Click en botón de generar ticket
+        navegador.findElement(By.xpath("//*[@id=\"btn-generate-ticket\"]")).click();
     }
 }
